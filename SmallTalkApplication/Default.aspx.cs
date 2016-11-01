efa22211e6221599fb86065e5dc22711a5908763
manish mallavarapu <manish101096@gmail.com>
2016-11-01 11:37:44 -0700

    Added Create Button functionality
    Signed-off-by: manish mallavarapu <manish101096@gmail.com>

--- SmallTalkApplication/Default.aspx.cs
+++ SmallTalkApplication/Default.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,6 +10,11 @@ public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+    }
+    public void onCreateClick(object sender, EventArgs e)
+    {
+       
 
     }
 }
